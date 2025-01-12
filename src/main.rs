28b6eced3ee30cf7dfe9fcbcd8cec1904fa47ac8
Dimitrios Rammos <61520191+drammos@users.noreply.github.com>
2025-01-12 14:16:54 +0200

    Update main, using threads and mutex and set timer for it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::path::Path;
+use std::{
+    path::Path,
+    sync::{Arc, Mutex},
+    thread,
+    time::Instant,
+};
 
 use clap::Parser;
-use dicom::object::open_file;
+use dicom::{dictionary_std::tags, object::open_file};
 use walkdir::WalkDir;
 
 use dicom_cli_utility::error::AppError;
@@ -17,36 +22,63 @@ fn main() {
     let args = Args::parse();
     let dicom_path = Path::new(&args.path);
 
-    // Counter for Dicom Files
-    let mut counter: i32 = 0;
+    // Set Timer
+    let start_time = Instant::now();
 
-    for entry in WalkDir::new(dicom_path).into_iter().filter_map(|e| e.ok()) {
-        let path = entry.path();
+    // Counter(mutex) and console_lock(mutex)
+    let counter = Arc::new(Mutex::new(0));
+    let console_lock = Arc::new(Mutex::new(()));
 
-        if !path.is_file() {
-            continue;
-        }
+    let mut handles = vec![];
 
-        extract_dicom(path)
-            .map(|_| counter += 1)
-            .map_err(|e| e.handle_error(path))
-            .ok();
+    for entry in WalkDir::new(dicom_path).into_iter().filter_map(Result::ok) {
+        let path = entry.path().to_path_buf();
+
+        // Copy counter and console_loc(mutex)
+        let thread_counter = Arc::clone(&counter);
+        let thread_console_lock = Arc::clone(&console_lock);
+
+        // Create new thread
+        let handle = thread::spawn(move || {
+            if path.is_file() {
+                match extract_dicom(&path, thread_console_lock) {
+                    Ok(_) => {
+                        // Increase the counter
+                        let mut counter = thread_counter.lock().unwrap();
+                        *counter += 1;
+                    }
+                    Err(e) => e.handle_error(&path),
+                }
+            }
+        });
+        handles.push(handle); // Add the handle in Vector
     }
 
-    println!("Total DICOM files processed: {}", counter);
+    // Wait all threads to join
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    let duration = start_time.elapsed();
+    println!(
+        "Total DICOM files processed: {} and time {:?}",
+        counter.lock().unwrap(),
+        duration
+    );
 }
 
 /// extract_dicom file, read a file and Print PatientName and PatientID for this path
-fn extract_dicom(path: &Path) -> Result<(), AppError> {
+fn extract_dicom(path: &Path, console_lock: Arc<Mutex<()>>) -> Result<(), AppError> {
     let object = open_file(path)?;
 
-    let patient_name = object.element_by_name("PatientName")?.to_str()?;
-    let patient_id = object.element_by_name("PatientID")?.to_str()?;
+    let patient_name = object.element(tags::PATIENT_NAME)?.to_str()?;
+    let patient_id = object.element(tags::PATIENT_ID)?.to_str()?;
 
+    // Lock the mutex
+    let _lock = console_lock.lock().unwrap();
     println!("Found DICOM file:");
     println!("File Path: {}", path.display());
     println!("Patient Name: {}", patient_name);
     println!("Patient ID: {}", patient_id);
-    println!("-------------------------------------------\n");
+    println!("------------------------------------------- \n");
     Ok(())
 }
